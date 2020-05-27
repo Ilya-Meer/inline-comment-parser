e8f9c91f95255c6674ff0f28bb44bd81e60091ce
Ilya Meerovich <ilya.meerov@gmail.com>
2020-05-26 20:58:18 -0700

    fix: adjust interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,23 @@
 export type ParserTransformation = (state: ParserState) => ParserState;
 
-export type ParserResult = string | string[] | null;
-
 export interface ParserState {
   target: string;
   index: number;
-  result: ParserResult;
+  result: any;
   isError: boolean | null;
   error: string | null;
+}
+
+export interface ParsedComment {
+  content: string;
+  source?: {
+    type: string;
+    name: string;
+    lineNumber: string;
+  };
+}
+
+export interface EnhancedComment {
+  file: string;
+  comments: ParsedComment[];
 }
